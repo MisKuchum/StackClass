58f305f6ec1319ba98787ff4c13a0e08bacd0f47
MisKuchum <61450683+MisKuchum@users.noreply.github.com>
2024-01-22 17:58:55 +0300

    Revert "StackClass modified"
    This reverts commit e54b519f9f448cf8b6120383c27b9731e1b39eac.

--- src/main/kotlin/MyStack.kt
+++ src/main/kotlin/MyStack.kt
@@ -1,23 +1,35 @@
 class MyStack<T>(
     rawItems: Collection<T> = listOf(),
 ) {
+
     // Список элементов стека
-    private var items: MutableList<T> = rawItems.toMutableList()
+    private var items: MutableList<T>
+
+    //Индекс последнего элемента стека
+    private var top: Int = -1
+
+    // Инициализирующий блок
+    init {
+        items = rawItems.toMutableList()
+        top = items.size - 1
+    }
 
     // Извлекает последний элемент
-    fun pop(): T? {
-        val res = items.lastOrNull() ?: return null
-        items.removeAt(items.size - 1)
+    fun pop(): T {
+        val res = items[top]
+        items.removeAt(top)
+        top--
         return res
     }
 
     // Добавить элемент
     fun push(element: T) {
         items.add(element)
+        top++
     }
 
     // Возвращает последний элемент
-    fun peek(): T? = items.lastOrNull()
+    fun peek(): T = items[top]
 
     // Проверка на пустой стек
     fun isEmpty(): Boolean = items.isEmpty()
